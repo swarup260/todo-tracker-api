c56450d88b9c96d5780ff239315d4a545e0ef8bc
swarup das <swarup260@gmail.com>
2020-10-02 00:11:06 +0530

    code formatted

--- api/helpers/function.helper.js
+++ api/helpers/function.helper.js
@@ -1,17 +1,23 @@
-
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 const userModel = require('../models/User.model');
 
-exports.getToken = data =>  jwt.sign({ username : data.username , email : data.email}, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRES });
+exports.getToken = data => jwt.sign({
+    username: data.username,
+    email: data.email
+}, config.JWT_SECRET, {
+    expiresIn: config.JWT_EXPIRES
+});
 
 exports.userExists = async data => {
-    const user = await userModel.findOne({email : data.email});
+    const user = await userModel.findOne({
+        email: data.email
+    });
     if (!user) {
         throw new Error("user not found");
     }
-    
-    if(!user.status){
+
+    if (!user.status) {
         throw new Error("user is  disable");
     }
     return await user;
