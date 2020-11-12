83b90bb317d48e7d8990008ae346df1e52085d73
swarup <swarup260@gmail.com>
2020-11-13 00:42:26 +0530

    removing api code

--- api/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT : 3000 || process.env.PORT,
-    MONGODB_URI: "mongodb://localhost:27017/TodoAppV2?retryWrites=true",
-    MONGODB_PORT : "27017",
-    JWT_SECRET: "JWT_SECRET",
-    JWT_EXPIRES : 60*60,
-    SALT_ROUNDS : 10
-}
